80c759d36e890846c8e0be375e39dae9bf40d89f
Aleena <caleenadenny@gmail.com>
2025-06-22 14:18:28 +0200

    Update TestDB.java

--- smart-library/src/main/java/com/library/TestDB.java
+++ smart-library/src/main/java/com/library/TestDB.java
@@ -10,7 +10,7 @@ import org.mindrot.jbcrypt.BCrypt;
 public class TestDB {
     private static final String URL = "jdbc:mysql://localhost:3306/library?useSSL=false";
     private static final String USER = "root";
-    private static final String PASSWORD = "MaryRome123!"; // Replace with your MySQL password
+    private static final String PASSWORD = "root"; // Replace with your MySQL password
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
